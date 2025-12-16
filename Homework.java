98ea9c970243ae343fd1d27f20e6cf740ae75004
robotminer12-cmd <robotminer12@gmail.com>
2025-12-16 17:49:55 +0530

    Add files via upload

---
+++ Homework.java
@@ -0,0 +1,14 @@
+public class Homework {
+
+    public static void main(String[] args) {
+        int nexus = 70;
+        int syndicate= 40;
+        int sum = nexus + syndicate;
+        int mult= nexus * syndicate;
+        int div= nexus % syndicate;
+        int difference =nexus - syndicate;
+        System.out.println(sum + difference + div + mult);
+        String game =("minecraft is fun");
+        System.out.println(game);
+    }
+}
---
+++ operators.java
@@ -0,0 +1,13 @@
+public class operators {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 5;
+        int sum = a + b;
+        int difference = a - b;
+        int mult = a * b;
+        int div = a / b;
+        System.out.println(sum + difference + mult + div);
+        String ball = "Messi is the goat";
+        System.out.println(ball);
+    }
+}
