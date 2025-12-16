dd72ec3a43e20a215c237135b56a8aa0a17061d9
robotminer12-cmd <robotminer12@gmail.com>
2025-12-16 18:23:17 +0530

    Add files via upload

---
+++ uninary.java
@@ -0,0 +1,17 @@
+public class uninary {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+System.out.println("Uninary operator operator "+(a++));
+ System.out.println("Uninary operator operator " + (++b));
+System.out.println("Binary operator");
+System.out.println("1+2 " +1+2);
+System.out.println("1+2 " +(1+2));
+System.out.println(1+2 +" =3");
+int increment =++a * b++;
+System.out.println(increment);
+System.out.println("ternary operators");
+int largestnumber = (a>b)?a:b;
+System.out.println("largest of 2 numbers" +largestnumber);
+    }
+}
