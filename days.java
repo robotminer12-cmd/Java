e61e434919564bc9b42bc33de2bf34bf4f37ce62
robotminer12-cmd <robotminer12@gmail.com>
2025-12-18 16:16:03 +0530

    Add files via upload

---
+++ days.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+public class days {
+    public static void main(String[]args) {
+Scanner sc = new Scanner (System.in);
+System.out.print("enter a number (1-7):");
+int day=sc.nextInt();
+switch (day){
+    case 1:
+    System.out.println("Today is Monday:");
+    break;
+    case 2:
+    System.out.println("Today is Tuesday:");
+    break;
+    case 3:
+    System.out.println("Today is Wednesday:");
+    break;
+    case 4:
+    System.out.println("Today is Thursday:");
+    break;
+    case 5:
+    System.out.println("Today is Friday:");
+    break;
+    case 6:
+    System.out.println("Today is Staurday:");
+    break;
+    case 7:
+    System.out.println("Today is Sunday:");
+    break;
+    default:
+    System.out.println("This is invalid please enter a number between 1-7");
+    
+}
+sc.close();
+
+
+    }
+    
+}
