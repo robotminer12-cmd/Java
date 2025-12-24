3af173abe9be41beda88993eaf070033beb8be79
robotminer12-cmd <robotminer12@gmail.com>
2025-12-24 16:34:06 +0530

    Add files via upload

---
+++ fibo.java
@@ -0,0 +1,13 @@
+public class fibo {
+    public static void main(String[]args) {
+        int a=0,b=1,c;
+        System.out.println(a+""+b+"");
+        for (int i=1;i<=8;i++) {
+c=a+b;
+System.out.print(c+"");
+a=b;
+b=c;
+        }
+
+    }
+}
