445335f088036203696acf5ab3555393ae749a98
robotminer12-cmd <robotminer12@gmail.com>
2025-12-29 15:27:19 +0530

    Add files via upload

---
+++ selectionsort.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class selectionsort {
+    public static void main (String[]args) {
+        Scanner sc=new Scanner(System.in);
+       
+        System.out.println("Enter number of elements:");
+   int n=sc.nextInt(n);
+    
+   int[]a=new int[n];
+
+   System.out.println("Enter array elements:");
+   for ( int i=0;1<n;i++) {
+   a[i]=sc.nextInt();
+
+   }
+ for (int)
